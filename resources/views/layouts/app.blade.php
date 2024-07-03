73361132525198abb45b1c08bbe2dbf7885d42dd
Dennis Erazo <dennis.erazo96@gmail.com>
2024-07-03 00:03:16 -0600

    Edicion del template dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- CSS Files -->
     <link id="pagestyle" href="assets/css/argon-dashboard.css" rel="stylesheet" />
+    <link  href="assets/css/custom.css" rel="stylesheet" />
 </head>
 
 <body class="{{ $class ?? '' }}">
@@ -42,7 +43,7 @@
                 <main class="main-content border-radius-lg">
                     @yield('content')
                 </main>
-            @include('components.fixed-plugin')
+            {{-- @include('components.fixed-plugin') --}}
         @endif
     @endauth
 
