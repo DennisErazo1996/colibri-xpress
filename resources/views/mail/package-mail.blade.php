bda816d203e7fb24377e1e5c13845b07f2f81846
Dennis Erazo <dennis.erazo96@gmail.com>
2024-10-08 14:58:11 -0600

    Correccion en correo de recepcion de paquete

--- resources/views/mail/package-mail.blade.php
+++ resources/views/mail/package-mail.blade.php
@@ -157,7 +157,7 @@ table.center-on-narrow {
                   <tr>
                     <td style="padding: 40px; text-align: center; font-family: 'Nunito Sans', sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #404040;">
                         Hola, {{$nombre}} ({{$lockerNumber}})<br><br>
-                        Hemos recibido tu paquete con tracking <strong>{{$numeroTracking}}</strong> en nuestra dirección de Bradenton, Florida. <br> <br>
+                        Hemos recibido tu paquete con tracking <strong>{{$numeroTracking}}</strong> en nuestra dirección de Miami, Florida. <br> <br>
                         Te recordamos que el próximo envío de paquetes es el <strong>{{$envio}}</strong>,
                         muchas gracias por confiar en nuestros servicios
                         <br>
