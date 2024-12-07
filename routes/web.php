f5de08c368edd1a6d8d0cad6161450e173174d3d
Dennis Erazo <dennis.erazo96@gmail.com>
2024-12-06 22:49:18 -0600

    Optimizacion de rutas en un grupo de middleware

--- routes/web.php
+++ routes/web.php
@@ -53,57 +53,60 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
 	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static'); 
 
-	//Endpoints pageController
-	Route::get('/caja/{idCaja}/paquetes', [PageController::class, 'paquetes']); 
-	Route::get('/cajas', [PageController::class, 'cajas'])->name('cajas')->middleware('checkUser');
-	Route::post('/crear-caja', [CajasController::class, 'crearCaja'])->name('crear-caja')->middleware('checkUser');
-	Route::get('/caja/{id}/pedidos', [PageController::class, 'pedidos'])->middleware('checkUser');
-	Route::get('/pedidos', [PageController::class, 'verPedidos'])->name('pedidos')->middleware('checkUser');
-	Route::post('/ver-pedidos/caja/{id}', [CajasController::class, 'pedidos'])->name('ver-pedidos')->middleware('checkUser');
-	Route::get('/ver-pedidos/caja/{id}/cliente/{idCliente}', [PageController::class, 'pedidosCliente'])->name('ver-pedidos-cliente')->middleware('checkUser');
-	Route::get('/productos', [PageController::class, 'verProductos'])->name('productos')->middleware('checkUser');;
-	Route::get('/clientes', [PageController::class, 'clientes'])->name('clientes')->middleware('checkUser');;
-	Route::get('/{page}', [PageController::class, 'index'])->name('page')->middleware('checkUser');
-	
-	//Endpoints cajasController
-	Route::post('/pedidos-cliente/caja/{id}/cliente/{idCliente}', [CajasController::class, 'verPedidosCliente'])->name('pedidos-cliente')->middleware('checkUser');
-	Route::post('/eliminar-pedido/caja/{idCaja}/cliente/{idCliente}', [CajasController::class, 'eliminarPedido'])->name('eliminar-pedido')->middleware('checkUser');
-	Route::post('/editar-pedido', [CajasController::class, 'editarPedido'])->name('editar-pedido')->middleware('checkUser');
-	Route::post('/crear-pedido', [CajasController::class, 'agregarPedido'])->middleware('checkUser');
-	Route::post('/eliminar-caja/{idCaja}', [CajasController::class, 'eliminarCaja'])->name('eliminar-caja')->middleware('checkUser');
-	Route::post('/editar-caja', [CajasController::class, 'editarCaja'])->name('editar-caja')->middleware('checkUser');
-	Route::post('/ver-paquetes/caja/{id}', [CajasController::class, 'verPaquetes'])->name('ver-paquetes')->middleware('checkUser');
-	Route::post('/registrar-paquete', [CajasController::class, 'registrarPaquete'])->middleware('checkUser');;
-	Route::post('/editar-paquete', [CajasController::class, 'editarPaquete'])->name('editar-paquete')->middleware('checkUser');
-	Route::post('/enviar-paquetes', [CajasController::class, 'enviarPaquetes'])->name('enviar-paquetes')->middleware('checkUser');
-	Route::post('/ver-paquetes-enviados/caja/{id}', [CajasController::class, 'verPaquetesEnviados'])->name('ver-paquetes-enviados')->middleware('checkUser');
-	Route::post('/guardar-envio', [CajasController::class, 'guardarDatosEnvios'])->name('guardar-envio')->middleware('checkUser');
-	Route::post('/datos-envio', [CajasController::class, 'datosTotalesEnvio'])->name('datos-envio')->middleware('checkUser');
-	Route::post('/actualizar-estado-pago', [CajasController::class, 'actualizarEstadoPago'])->name('estado-pago')->middleware('checkUser');
-	
-	//Endpoints ventasController
-	Route::post('/ver-clientes', [VentasController::class, 'verClientes'])->name('ver-clientes')->middleware('checkUser');
-	Route::post('/registrar-cliente', [VentasController::class, 'registrarCliente'])->name('registrar-cliente')->middleware('checkUser');
-	Route::post('/ver-productos', [VentasController::class, 'verProductos'])->name('ver-productos')->middleware('checkUser');
-	Route::post('/registrar-producto', [VentasController::class, 'registrarProducto'])->name('registrar-producto')->middleware('checkUser');
-	Route::post('/registrar-venta', [VentasController::class, 'registrarVenta'])->name('registrar-venta')->middleware('checkUser');
-	Route::post('/ver-ventas', [VentasController::class, 'verVentas'])->name('ver-ventas')->middleware('checkUser');
-	Route::post('/ver-creditos', [VentasController::class, 'verCreditos'])->name('ver-creditos')->middleware('checkUser');
-	Route::post('/ver-total-productos', [VentasController::class, 'datosTotalesProductos'])->name('total-productos')->middleware('checkUser');
-	Route::post('/ver-total-ventas', [VentasController::class, 'datosTotalesVentas'])->name('total-ventas')->middleware('checkUser');
-	Route::post('/editar-producto', [VentasController::class, 'editarProducto'])->name('editar-producto')->middleware('checkUser');
-	Route::post('/eliminar-venta', [VentasController::class, 'eliminarVenta'])->name('eliminar-venta')->middleware('checkUser');
-	Route::post('/eliminar-producto', [VentasController::class, 'eliminarProducto'])->name('eliminar-producto')->middleware('checkUser');
-	Route::post('/eliminar-credito', [VentasController::class, 'eliminarCredito'])->name('eliminar-credito')->middleware('checkUser');
-	Route::post('/ver-total-creditos', [VentasController::class, 'datosTotalesCreditos'])->name('total-creditos')->middleware('checkUser');
-	Route::post('/registrar-cuota', [VentasController::class, 'registrarCuota'])->name('registrar-cuota')->middleware('checkUser');
-	Route::post('/ver-cuotas-credito', [VentasController::class, 'verCuotasCredito'])->name('ver-cuotas-credito')->middleware('checkUser');
-	Route::post('/eliminar-cuota', [VentasController::class, 'eliminarCuota'])->name('eliminar-cuota')->middleware('checkUser');
-	Route::post('/editar-cliente', [VentasController::class, 'editarCliente'])->name('editar-cliente')->middleware('checkUser');
-	Route::post('/eliminar-cliente', [VentasController::class, 'eliminarCliente'])->name('eliminar-cliente')->middleware('checkUser');
-	Route::post('/liquidar-ventas', [VentasController::class, 'liquidarVentas'])->name('liquidar-ventas')->middleware('checkUser');
-	Route::post('/liquidar-creditos', [VentasController::class, 'liquidarCreditos'])->name('liquidar-creditos')->middleware('checkUser');
+	Route::middleware('checkUser')->group(function () {
 
+		//Endpoints pageController
+		Route::get('/caja/{idCaja}/paquetes', [PageController::class, 'paquetes']); 
+		Route::get('/cajas', [PageController::class, 'cajas'])->name('cajas');
+		Route::post('/crear-caja', [CajasController::class, 'crearCaja'])->name('crear-caja');
+		Route::get('/caja/{id}/pedidos', [PageController::class, 'pedidos']);
+		Route::get('/pedidos', [PageController::class, 'verPedidos'])->name('pedidos');
+		Route::post('/ver-pedidos/caja/{id}', [CajasController::class, 'pedidos'])->name('ver-pedidos');
+		Route::get('/ver-pedidos/caja/{id}/cliente/{idCliente}', [PageController::class, 'pedidosCliente'])->name('ver-pedidos-cliente');
+		Route::get('/productos', [PageController::class, 'verProductos'])->name('productos')->middleware('checkUser');;
+		Route::get('/clientes', [PageController::class, 'clientes'])->name('clientes')->middleware('checkUser');;
+		Route::get('/{page}', [PageController::class, 'index'])->name('page')->middleware('checkUser');
+		
+		//Endpoints cajasController
+		Route::post('/pedidos-cliente/caja/{id}/cliente/{idCliente}', [CajasController::class, 'verPedidosCliente'])->name('pedidos-cliente');
+		Route::post('/eliminar-pedido/caja/{idCaja}/cliente/{idCliente}', [CajasController::class, 'eliminarPedido'])->name('eliminar-pedido');
+		Route::post('/editar-pedido', [CajasController::class, 'editarPedido'])->name('editar-pedido');
+		Route::post('/crear-pedido', [CajasController::class, 'agregarPedido']);
+		Route::post('/eliminar-caja/{idCaja}', [CajasController::class, 'eliminarCaja'])->name('eliminar-caja');
+		Route::post('/editar-caja', [CajasController::class, 'editarCaja'])->name('editar-caja')->middleware('checkUser');
+		Route::post('/ver-paquetes/caja/{id}', [CajasController::class, 'verPaquetes'])->name('ver-paquetes');
+		Route::post('/registrar-paquete', [CajasController::class, 'registrarPaquete'])->middleware('checkUser');;
+		Route::post('/editar-paquete', [CajasController::class, 'editarPaquete'])->name('editar-paquete');
+		Route::post('/enviar-paquetes', [CajasController::class, 'enviarPaquetes'])->name('enviar-paquetes');
+		Route::post('/ver-paquetes-enviados/caja/{id}', [CajasController::class, 'verPaquetesEnviados'])->name('ver-paquetes-enviados');
+		Route::post('/guardar-envio', [CajasController::class, 'guardarDatosEnvios'])->name('guardar-envio');
+		Route::post('/datos-envio', [CajasController::class, 'datosTotalesEnvio'])->name('datos-envio');
+		Route::post('/actualizar-estado-pago', [CajasController::class, 'actualizarEstadoPago'])->name('estado-pago');
+		
+		//Endpoints ventasController
+		Route::post('/ver-clientes', [VentasController::class, 'verClientes'])->name('ver-clientes');
+		Route::post('/registrar-cliente', [VentasController::class, 'registrarCliente'])->name('registrar-cliente');
+		Route::post('/ver-productos', [VentasController::class, 'verProductos'])->name('ver-productos');
+		Route::post('/registrar-producto', [VentasController::class, 'registrarProducto'])->name('registrar-producto');
+		Route::post('/registrar-venta', [VentasController::class, 'registrarVenta'])->name('registrar-venta');
+		Route::post('/ver-ventas', [VentasController::class, 'verVentas'])->name('ver-ventas');
+		Route::post('/ver-creditos', [VentasController::class, 'verCreditos'])->name('ver-creditos');
+		Route::post('/ver-total-productos', [VentasController::class, 'datosTotalesProductos'])->name('total-productos');
+		Route::post('/ver-total-ventas', [VentasController::class, 'datosTotalesVentas'])->name('total-ventas');
+		Route::post('/editar-producto', [VentasController::class, 'editarProducto'])->name('editar-producto');
+		Route::post('/eliminar-venta', [VentasController::class, 'eliminarVenta'])->name('eliminar-venta');
+		Route::post('/eliminar-producto', [VentasController::class, 'eliminarProducto'])->name('eliminar-producto');
+		Route::post('/eliminar-credito', [VentasController::class, 'eliminarCredito'])->name('eliminar-credito');
+		Route::post('/ver-total-creditos', [VentasController::class, 'datosTotalesCreditos'])->name('total-creditos');
+		Route::post('/registrar-cuota', [VentasController::class, 'registrarCuota'])->name('registrar-cuota');
+		Route::post('/ver-cuotas-credito', [VentasController::class, 'verCuotasCredito'])->name('ver-cuotas-credito')->middleware('checkUser');
+		Route::post('/eliminar-cuota', [VentasController::class, 'eliminarCuota'])->name('eliminar-cuota');
+		Route::post('/editar-cliente', [VentasController::class, 'editarCliente'])->name('editar-cliente');
+		Route::post('/eliminar-cliente', [VentasController::class, 'eliminarCliente'])->name('eliminar-cliente');
+		Route::post('/liquidar-ventas', [VentasController::class, 'liquidarVentas'])->name('liquidar-ventas');
+		Route::post('/liquidar-creditos', [VentasController::class, 'liquidarCreditos'])->name('liquidar-creditos');
+		
+	});
 	
 	Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 });
