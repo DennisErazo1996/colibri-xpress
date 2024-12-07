d0f6b61e13edfc414b217a50d29a154544c724af
Dennis Erazo <dennis.erazo96@gmail.com>
2024-12-06 18:55:38 -0600

    Creacion de middleware para usuario

---
+++ app/Http/Middleware/CheckUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->id === 1 && Auth::user()->id === 19) { // Cambia 1 por el ID del usuario permitido
+            return $next($request);
+        }
+
+        // Redirige o muestra un error si no tiene acceso
+        return redirect('/')->with('error', 'No tienes acceso a esta sección.');
+
+        //return $next($request);
+    }
+}
