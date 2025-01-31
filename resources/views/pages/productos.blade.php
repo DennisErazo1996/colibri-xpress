87fbb7eff1186375b373ddbf48b9ddcd2010b0b7
Dennis Erazo <dennis.erazo96@gmail.com>
2025-01-31 12:58:44 -0600

    Guardar estado del inversor

--- resources/views/pages/productos.blade.php
+++ resources/views/pages/productos.blade.php
@@ -105,18 +105,25 @@
 
                 <div class="card mb-0 p-2" id="card-list-productos">
                     <div class="text-center d-flex flex-row justify-content-center col-md-2">
-                        
+
                         @if (Auth::user()->role == 'super-admin')
                             <div
                                 class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                                 <i class="ni ni-circle-08 text-dark text-sm opacity-10"></i>
                             </div>
 
-                            <select name="investor_id" id="investor_id" class="form-control"
-                                onchange="cambiarInversor(this.value)">
-                                <option selected disabled>Seleccione el inversionista</option>
+                            <select name="investor_id" id="investor_id" class="form-control" onchange="cambiarInversor(this.value)">
+                                <option value="" disabled
+                                    @if (session('id_inversor') == null)
+                                        selected
+                                    @endif
+                                >Seleccione el inversionista</option>
                                 @foreach ($inversores as $inv)
-                                    <option value="{{ $inv->id }}">{{ $inv->firstname }}</option>
+                                    <option value="{{ $inv->id }}"
+                                        @if (session('id_inversor') == $inv->id)
+                                            selected
+                                        @endif
+                                    >{{ $inv->firstname.' '.$inv->lastname }}</option>
                                 @endforeach
                             </select>
                         @endif
